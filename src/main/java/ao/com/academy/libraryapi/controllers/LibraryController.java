d048217221673f4a07cd149e8887c70e8b9e22b7
Dário Silva <dariosilva13222@gmail.com>
2024-09-17 22:11:29 +0100

    feat: adds endpoints for create and read operations

--- src/main/java/ao/com/academy/libraryapi/controllers/LibraryController.java
+++ src/main/java/ao/com/academy/libraryapi/controllers/LibraryController.java
@@ -1,11 +1,44 @@
 package ao.com.academy.libraryapi.controllers;
 
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+
+import ao.com.academy.libraryapi.bookdto.BookDTO;
+import ao.com.academy.libraryapi.exceptions.BookNotFoundException;
+import ao.com.academy.libraryapi.services.BookService;
 
 
 @RestController
 @RequestMapping
 public class LibraryController {
     
+    @Autowired
+    BookService bookService;
+
+    // Get all books
+    @GetMapping("/library/books")
+    public List<BookDTO> getAllBooks(){
+        return bookService.getBooks();
+    }
+
+    // Find book by Id
+    @GetMapping("/library/books/{id}")
+    public BookDTO getBookById(@PathVariable(name="id") Long bookId) throws BookNotFoundException{
+        return bookService.getBookById(bookId);
+    }
+
+    // Create Book
+    @PostMapping("/library/books/new")
+    public ResponseEntity<?> createBook(@RequestBody BookDTO book){
+        return bookService.createBook(book);
+    }
+
 }
