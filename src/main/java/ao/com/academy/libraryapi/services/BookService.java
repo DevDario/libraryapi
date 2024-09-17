2b605e8f4228f25f7ac0a0c2593ee6ef35822c52
Dário Silva <dariosilva13222@gmail.com>
2024-09-17 21:27:22 +0100

    raw: fetches all books and also by id
    raw: throws custom Book not found exception if no book where found

--- src/main/java/ao/com/academy/libraryapi/services/BookService.java
+++ src/main/java/ao/com/academy/libraryapi/services/BookService.java
@@ -1,5 +1,24 @@
 package ao.com.academy.libraryapi.services;
 
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+
+import ao.com.academy.libraryapi.bookdto.BookDTO;
+import ao.com.academy.libraryapi.exceptions.BookNotFoundException;
+import ao.com.academy.libraryapi.repositories.BookRepository;
+
 public class BookService {
     
+
+    @Autowired
+    BookRepository bookRepository;
+
+    public List<BookDTO> getBooks() throws BookNotFoundException{
+        return bookRepository.findAll();
+    }
+
+    public BookDTO getBookById(Long bookId) throws BookNotFoundException{
+        return bookRepository.findById(bookId).orElseThrow(() -> new BookNotFoundException(bookId));
+    }
 }
