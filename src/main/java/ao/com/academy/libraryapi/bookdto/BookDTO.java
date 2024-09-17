b790997b8633b22ee2fbbbbe4da3317050a22904
Dário Silva <dariosilva13222@gmail.com>
2024-09-17 20:59:43 +0100

    feat: adds persistence-related decorators to attributes

--- src/main/java/ao/com/academy/libraryapi/bookdto/BookDTO.java
+++ src/main/java/ao/com/academy/libraryapi/bookdto/BookDTO.java
@@ -1,6 +1,9 @@
 package ao.com.academy.libraryapi.bookdto;
 
+import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.Id;
 import jakarta.persistence.Table;
 import lombok.AllArgsConstructor;
 import lombok.Getter;
@@ -14,7 +17,18 @@ import lombok.Setter;
 @Getter
 @Setter
 public class BookDTO {
+    
+    @Id
+    @GeneratedValue
+    @Column(name="id")
+    private Long bookId;
+
+    @Column(name="bookName")
     private String bookName;
+
+    @Column(name="bookAuthor")
     private String bookAuthor;
+
+    @Column(name="releaseDate")
     private String releaseDate;
 }
