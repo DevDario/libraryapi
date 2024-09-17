d155a52166257934750a6ca429f9cb703351f754
Dário Silva <dariosilva13222@gmail.com>
2024-09-17 21:08:41 +0100

    feat: creates controller class

---
+++ src/main/java/ao/com/academy/libraryapi/controllers/LibraryController.java
@@ -0,0 +1,11 @@
+package ao.com.academy.libraryapi.controllers;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+
+@RestController
+@RequestMapping
+public class LibraryController {
+    
+}
