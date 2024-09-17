edd1066618c9e6c1320e16e94ef52ced443fbf3d
Dário Silva <dariosilva13222@gmail.com>
2024-09-17 22:24:45 +0100

    fix: fixes primary key name

--- src/main/resources/db/migrations/V1__create_books_table.sql
+++ src/main/resources/db/migrations/V1__create_books_table.sql
@@ -1,5 +1,5 @@
 CREATE TABLE books(
-    boookId BIGINT PRIMARY KEY,
+    bookId BIGINT PRIMARY KEY,
     bookName VARCHAR(100) NOT NULL UNIQUE,
     bookAuthor VARCHAR(45) NOT NULL,
     releaseDate TIMESTAMP NOT NULL
