5cf840d55be56f15cbf556880b40a36d2229fe0d
Dário Silva <dariosilva13222@gmail.com>
2024-09-18 09:14:28 +0100

    feat: deletes book from db based on his id

--- src/main/java/ao/com/academy/libraryapi/services/BookService.java
+++ src/main/java/ao/com/academy/libraryapi/services/BookService.java
@@ -17,16 +17,29 @@ public class BookService {
     @Autowired
     BookRepository bookRepository;
 
+    // Gets all books from DB
     public List<Book> getBooks(){
         return bookRepository.findAll();
     }
 
+    // Find a book by id
     public Book getBookById(Long bookId) throws BookNotFoundException{
         return bookRepository.findById(bookId).orElseThrow(() -> new BookNotFoundException(bookId));
     }
 
+    // Stores a new book
     public ResponseEntity<?> createBook(Book book){
         bookRepository.save(book);
+
+        return ResponseEntity.ok().build();
+    }
+
+    // Delete a book
+    public ResponseEntity<?> deleteBook(Long bookId) throws BookNotFoundException{
+
+        Book book = bookRepository.findById(bookId).orElseThrow(() -> new BookNotFoundException(bookId));
+
+        bookRepository.delete(book);
 
         return ResponseEntity.ok().build();
     }
